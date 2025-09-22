aa074a37dc6b8df9ded79c212352cf840aba0694
dominjav43 <dominjav43@gmail.com>
2025-09-22 11:38:12 +0800

    use repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dominjav43/', // This is important for GitHub Pages
+  base: '/dominjav43.github.io/', // This is important for GitHub Pages
 });
